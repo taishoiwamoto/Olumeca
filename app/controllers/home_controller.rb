61ac82ef3c0425ac68718338d9694de3d09721be
TaishoIwamoto <h.m.tai.26@gmail.com>
2023-08-21 18:32:52 -0600

    changed the top page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :forbid_login_user, {only: [:top]}
 
   def top
+    @services = Service.order(created_at: :desc).limit(4)
   end
 
   def about
