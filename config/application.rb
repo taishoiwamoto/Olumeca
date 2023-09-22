3384c09cc0ecac095df026375ba442c6aa0690d7
TaishoIwamoto <h.m.tai.26@gmail.com>
2023-09-21 18:28:48 -0600

    changed styling

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module TweetApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :es
+    config.time_zone = "America/Belize"
   end
 end
