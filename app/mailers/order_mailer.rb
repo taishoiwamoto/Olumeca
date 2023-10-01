aff609855842094e8500559316b4d95f34ffe31e
TaishoIwamoto <h.m.tai.26@gmail.com>
2023-10-01 16:45:38 -0600

    changed email text

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,17 +2,17 @@ class OrderMailer < ApplicationMailer
   def order_notification
     @order = params[:order]
     @seller = User.find(@order.seller_id)
-    mail(to: @seller.email, subject: 'Se ha realizado un nuevo pedido.')
+    mail(to: @seller.email, subject: '¡Se ha realizado un nuevo pedido!')
   end
 
-  def order_status_notification
+  def order_status_notificatgition
     @order = params[:order]
     @buyer = User.find(@order.buyer_id)
     @seller = User.find(@order.seller_id)
     subject = if @order.accepted?
-                'Tu pedido ha sido aceptado.'
+                '¡Tu pedido ha sido aceptado!                '
               else
-                'Tu pedido ha sido rechazado.'
+                'Perdón, Tu pedido ha sido rechazado.'
               end
     mail(to: @buyer.email, subject: subject)
   end
