83ff64762bfa4d095696eb41d1cce2daa5ca0b38
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-10-09 00:40:51 +0000

    feat: remove comments and unnecsary method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,17 +12,14 @@ class UsersController < ApplicationController
   end
 
   def likes
-    # set_services_and_rating_for
     @likes = Like.joins(:service).merge(Service.active).where(user_id: current_user.id).order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def orders
-    # set_services_and_rating_for
     @orders = Order.where(buyer_id: current_user.id).order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def sales
-    # set_services_and_rating_for
     @sales = Order.where(seller_id: current_user.id).order(created_at: :desc).page(params[:page]).per(5)
   end
 
@@ -31,10 +28,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  # def set_services_and_rating_for
-  #   @average_rating = 0 # total_count > 0 ? total_reviews / total_count.to_f : nil
-  # end
 
   def set_user
     @user = User.active.find(params[:id])
