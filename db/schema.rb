244afb0f4215dfeb0f62e0284a43c378617f11a1
TaishoIwamoto <h.m.tai.26@gmail.com>
2023-10-10 16:10:14 -0600

    deleted delivery method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_10_215228) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_220434) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,12 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_10_215228) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "delivery_methods", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
