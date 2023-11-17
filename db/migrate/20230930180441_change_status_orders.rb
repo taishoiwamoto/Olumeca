369652db27936649f486cc9e394ab99993df2156
MrJuneJuneJune <enuj.enuj.enuj@gmail.com>
2023-11-17 14:13:35 +0900

    Fix migrate for deploy and use postgresql

--- db/migrate/20230930180441_change_status_orders.rb
+++ db/migrate/20230930180441_change_status_orders.rb
@@ -1,5 +1,5 @@
 class ChangeStatusOrders < ActiveRecord::Migration[7.0]
   def change
-  	change_column :orders, :status, :integer, default: 0
+  	change_column :orders, :status, :integer, default: 0, using: 'status::integer'
   end
 end
