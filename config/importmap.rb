69af1c954e2f7675259e3f15f53a6a2008ae61e8
Taisho Iwamoto <taishoiwamoto@TaishonoMacBook-Pro.local>
2024-04-08 22:07:46 -0600

    deleted

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
