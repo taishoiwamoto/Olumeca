a49a03e3a6cad5f81e62da1b7afde8aae7d0be74
Taisho Iwamoto <taishoiwamoto@TaishonoMacBook-Pro.local>
2024-04-09 14:14:51 -0600

    fixed recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key   = '6LedoAgoAAAAACiRtGAbzOqpCqXqVOVRjP3ClyV9'
-  config.secret_key = '6LedoAgoAAAAALtq7xsGPKwURRbuD1osnBVHOqhz'
+  config.site_key   = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
