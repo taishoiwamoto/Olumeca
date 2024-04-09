321759ce585b1e4adc2ff461fa0c5adf74475ba9
Taisho Iwamoto <taishoiwamoto@TaishonoMacBook-Pro.local>
2024-04-08 18:50:37 -0600

    fixed some part

--- Gemfile
+++ Gemfile
@@ -51,7 +51,7 @@ group :development, :test do
   gem "sqlite3", "~> 1.4"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem 'dotenv-rails'
+  gem 'dotenv-rails', groups: [:development, :test]
   gem 'hirb'
 end
 
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,6 +3,6 @@
 #これにより、スパムや不正なアクセスからウェブサイトを保護することができます。
 
 Recaptcha.configure do |config|
-  config.site_key   = '6LedoAgoAAAAACiRtGAbzOqpCqXqVOVRjP3ClyV9'
-  config.secret_key = '6LedoAgoAAAAALtq7xsGPKwURRbuD1osnBVHOqhz'
+  config.site_key   = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
