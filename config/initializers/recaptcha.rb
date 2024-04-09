91bdfcaadf225ba74183bf1953e4b82ec3c7c2f2
Taisho Iwamoto <taishoiwamoto@TaishonoMacBook-Pro.local>
2024-04-09 14:37:23 -0600

    fixed

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  ENV['RECAPTCHA_SITE_KEY']   = Rails.application.credentials.recaptcha[:site_key]
-  ENV['RECAPTCHA_SECRET_KEY'] = Rails.application.credentials.recaptcha[:secret_key]
+  config.site_key   = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
